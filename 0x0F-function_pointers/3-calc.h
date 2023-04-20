31e55917b91cd519c9e1a8e1a073f5107d67f331
NullCode1 <mosalah01229@gmail.com>
2023-04-20 00:02:21 +0000

    done

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,5 +1,9 @@
 #ifndef CALC_H
 #define CALC_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
 /**
  * struct op - A struct op.
  * @op: The operator.
@@ -18,4 +22,5 @@ int op_div(int a, int b);
 int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
 
-#end
+#endif
+
--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,6 +1,5 @@
 #include "3-calc.h"
 #include <stdlib.h>
-
 /**
  * get_op_func - Selects the correct function to perform
  *               the operation asked by the user.
@@ -9,7 +8,6 @@
  * Return: A pointer to the function corresponding
  *         to the operator given as a parameter.
  */
-
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
