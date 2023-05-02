10980743bf3b595d038aff925999391c30f69994
NullCode1 <mosalah01229@gmail.com>
2023-05-02 22:12:32 +0000

    done

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -3,22 +3,20 @@
 /**
  * free_listint2 - frees a linked list
  * @head: pointer to the listint_t list to be freed
- *
- * Return: void
  */
-
 void free_listint2(listint_t **head)
 {
-        listint_t *temp;
+listint_t *temp;
 
-        if (head == NULL)
-                return;
+if (head == NULL)
+return;
 
-        while (*head)
-        {
-                temp = (*head)->next;
-                free(*head);
-		*head = temp
-        }
-        *head = NULL;
+while (*head)
+{
+temp = (*head)->next;
+free(*head);
+*head = temp;
 }
+*head = NULL;
+}
+
