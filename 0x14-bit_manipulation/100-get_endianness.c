e3dbccbb274944cf177606aa76136617d8e0fe41
NullCode1 <mosalah01229@gmail.com>
2023-05-05 00:53:40 +0000

    done

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -2,11 +2,12 @@
 
 /**
  * get_endianness - checks if a machine is little or big endian
- * Return: 0 for big, 1 for little
+ *
+ * Return: 0 big endian, 1 if small
  */
 int get_endianness(void)
 {
-	unsigned long int i = 1;
+	unsigned long int n = 1;
 
 	return (*(char *)&n);
 }
