41c5d481f23c54b2c007c36e4266352aa9274cf1
NullCode1 <mosalah01229@gmail.com>
2023-05-05 00:31:33 +0000

    done

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,7 +8,7 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int user = 0;
+	unsigned int num = 0;
 
 	if (!b)
 		return (0);
@@ -17,7 +17,7 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if (*b != '0' && *b != '1')
 			return (0);
-		user = user  * 2 + (*b++ - '0');
+		num = num  * 2 + (*b++ - '0');
 	}
-	return (user);
+	return (num);
 }
