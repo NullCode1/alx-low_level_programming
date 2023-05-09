3fa45af4a4dc254678d37408792a9ed797ba4945
NullCode1 <mosalah01229@gmail.com>
2023-05-09 00:04:28 +0000

    done

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read_textfile- Read text file print to STDOUT.
+ * @filename: text file being read
+ * @letters: number of letters to be read
+ * Return: w- actual number of bytes read and printed
+ *        0 when function fails or filename is NULL.
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *user;
+	ssize_t x;
+	ssize_t w;
+	ssize_t t;
+
+	x = open(filename, O_RDONLY);
+	if (x == -1)
+		return (0);
+	user = malloc(sizeof(char) * letters);
+	t = read(x, user, letters);
+	w = write(STDOUT_FILENO, user, t);
+
+	free(user);
+	close(x);
+	return (w);
+}
